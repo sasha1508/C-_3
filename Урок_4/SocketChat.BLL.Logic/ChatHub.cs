9ab190e65db74834287d2d447ef41b0fbba8bd6d
Александр Ильин <sasha8@list.ru>
2024-04-15 20:38:11 +0300

    Урок 4 выполнен

---
+++ Урок_4/SocketChat.BLL.Logic/ChatHub.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SocketChat.Common.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketChat.BLL.Logic
+{
+    public class ChatHub : Hub
+    {
+
+        public async Task Send(SignalRMessage message)
+        {
+            await this.Clients.All.SendAsync("Receive", message.FromUser, message.Message);
+        }
+
+        public async Task SendToUser(SignalRMessage message)
+        {
+            var client = Clients.Client(message.ConnectionId);
+            await client.SendAsync($"message: {message.Message}. From user: {message.FromUser}");
+        }
+
+    }
+}
---
+++ Урок_4/SocketChat/Controllers/UserController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SocketChat.BLL.Logic;
+using SocketChat.Common.Entities;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SocketChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserLogic _userLogic;
+
+        public UserController(IUserLogic userLogic)
+        {
+            _userLogic = userLogic;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            var users = _userLogic.GetAll();
+
+            var user = users.FirstOrDefault(u => u.Name == "Bob");
+
+            return users;
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public void Post([FromBody] User user)
+        {
+            _userLogic.Add(user);
+        }
+    }
+}
