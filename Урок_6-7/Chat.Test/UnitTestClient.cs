7844c0b1c4d1620d8b878ed43f1ff6931746d083
Александр Ильин <sasha8@list.ru>
2024-04-30 15:27:35 +0300

    Добавлены тесты

---
+++ Урок_6-7/Chat.Test/UnitTestClient.cs
@@ -0,0 +1,37 @@
+﻿using ChatApp;
+using ChatDB;
+using ChatNetwork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chat.Test
+{
+    internal class UnitTestClient
+    {
+        [Test]
+        public void test1()
+        {
+           ChatClient chatClient = new();
+        }
+
+        [Test]
+        public async Task test2()
+        {
+            var mock = new MockMessagesSource();
+            var server = new ChatServer(mock);
+            mock.InitializeServer(server);
+            await server.WorkAsync();
+
+
+            var chatClient = new ChatClient();
+            chatClient.ReadMessage();
+
+
+        }
+
+        
+    }
+}
